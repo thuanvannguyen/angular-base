777686d58108b44c1bf391e2b5854f4e0de28cf1
thuan.nv <nguyenthuankma@gmail.com>
2024-09-21 10:11:27 +0700

    Update edit component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-started';
+  title = 'Hello 5000 ae!';
+
+  sayHello() {
+    alert('Hello App component');
+  }
 }
